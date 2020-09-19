fc9a38131a702cf3d94bd8cf375a4bd6913b9908
aaronthefknpanda <64624240+aaronthefknpanda@users.noreply.github.com>
2020-09-19 19:59:45 -0400

    AaronTest Scene
    AaronTest Scene created.
    
    Scripts:
    Created TestSpawnBox script.
    - Box prefabs spawn at randomly selected X positions.
    - Box prefabs spawn every set amount of time.
    Created TestBox script.
    - Box prefabs are destroyed when they touch the floor.
    
    Other:
    Created a Box prefab.

---
+++ AmazonSource/Assets/Scripts/Object/TestBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBox : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            Destroy(gameObject, 0.2f);          //When the box touches the floor, it'll be destroyed after 0.2 "seconds".
+        }
+
+        if (collision.gameObject.tag == "Character")
+        {
+            //Deal damage to the Character. We can also have the damage script on the Character instead.
+        }
+    }
+}
---
+++ AmazonSource/Assets/Scripts/Object/TestSpawnBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TestSpawnBox : MonoBehaviour
+{
+    public GameObject box;              //The object/prefab that will spawn.
+
+    //FOR TESTING PURPOSES - I know that we'll be using Screen Positions instead of this.    
+    private GameObject[] spawnPoints;   //An array for the Spawn Points. We'll be able to add and remove as many as needed.
+    private int randomPosition;         //Each Spawn Point as a integer. This helps when randomly selecting one of the Spawn Points.
+    private GameObject spawnPosition;   //The position of the randomly selected Spawn Point.
+
+    void Start()
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("Spawn");       //This finds all the objects tagged as "Spawn"; usually the Spawn Points.
+        InvokeRepeating("SpawnBox", 0.0f, 0.7f);                        //Every 0.7 "seconds", the function "Spawn Box" will trigger.
+    }
+
+    void SpawnBox()
+    {
+        randomPosition = Random.Range(0, spawnPoints.Length);           //This randomly selects one of the Spawn Points and stores its position.
+        spawnPosition = spawnPoints[randomPosition];                    //This takes the randomly chosen Spawn Point and find it's transform.
+        Instantiate(box, spawnPosition.transform);                      //This spawns a box at transform of the aforementioned Spawn Point.
+    }
+}
